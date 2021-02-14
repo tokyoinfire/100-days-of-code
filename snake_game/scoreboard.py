1b0d6ef19ae56a362363b0dc63f46973fdda3a4f
tokyoinfire <sveklenko15@gmail.com>
2021-02-14 15:04:45 +0300

    snake game highscore update

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = self.get_high_score()
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,13 +16,27 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", False, align = ALIGNMENT, font = FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False, align = ALIGNMENT, font = FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", False, align = ALIGNMENT, font = FONT)
+    def reset(self):
+        if self.score > int(self.high_score):
+            self.high_score = self.score
+            with open("data.txt", mode = "w") as file:
+                file.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
+
+    def get_high_score(self):
+        with open("data.txt") as file:
+            high_score = int(file.read())
+        return high_score
+
+    def set_high_score(self, score):
+        with open("data.txt", mode = "w") as file:
+            file.write(f"{score}")
+
