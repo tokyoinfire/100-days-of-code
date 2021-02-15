a09319779a52b3c990f4d7fb5446d71afe26f7b7
tokyoinfire <sveklenko15@gmail.com>
2021-02-15 22:29:02 +0300

    mail merge project done

---
+++ mail_merge_project/main.py
@@ -0,0 +1,12 @@
+PLACEHOLDER = "[name]"
+
+with open("./Input/Names/invited_names.txt") as file:
+    names = file.readlines()
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_content = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_content.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}", "w") as completed_letter:
+            completed_letter.write(new_letter)
