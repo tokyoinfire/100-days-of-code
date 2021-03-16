cd00807f1a96aae8bd38d376a01bf7fbe12cded1
tokyoinfire <sveklenko15@gmail.com>
2021-03-16 19:20:56 +0300

    just a little bit of everything

---
+++ day6.py
@@ -0,0 +1,16 @@
+#print(len(input("What's your name?")))
+
+# a = input("a:")
+# b = input("b:")
+#
+# temp = a
+# a = b
+# b = temp
+#
+# print("a:" + a)
+# print("b:" + b)
+
+print("Welcome to the Band Name Generator.")
+city = input("What's name of the city you grew up in?\n")
+pet = input("what's your pet's name?\n")
+print("Your band name could be " + city + " " + pet)
--- nato-alphabet/main.py
+++ nato-alphabet/main.py
@@ -4,7 +4,16 @@ nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 
 nato_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
 
-name = input("Enter a word: ").upper()
-result = [nato_dict[letter] for letter in name]
 
-print(result)
+def generate_phonetic():
+    name = input("Enter a word: ").upper()
+
+    try:
+        result = [nato_dict[letter] for letter in name]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please")
+        generate_phonetic()
+    else:
+        print(result)
+
+generate_phonetic()
